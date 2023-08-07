aacb1390e6f62e917dfbf291cb057cc1efe168a4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-07 16:22:05 +0300

    update Hero.tsx

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -5,13 +5,13 @@ const Hero = () => {
   return (
     <>
       <div className="bg-center bg-auto bg-[url('/hero.jpg')] p-4">
-        <div className="flex flex-col py-12">
+        <div className="flex flex-col py-12 max-w-xs">
           <h3 className="subheading">welcome to pixoto world</h3>
           <h1 className="heading">We are a digital photography house</h1>
           <p className="paragraph">
             Discover the world of digital photography with Pixoto. We specialize
             in capturing moments that matter, turning them into lasting
-            memories. Explore our gallery and let images speak.{" "}
+            memories. Explore our gallery and let images speak.
           </p>
           <div className="flex flex-col gap-3 font-medium">
             <button className="btn btn-fill">
