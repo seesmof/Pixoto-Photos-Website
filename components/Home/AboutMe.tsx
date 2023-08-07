724e201e76d34a86f0f095fdf75cf31c39c91fcd
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-07 16:19:27 +0300

    update globals.css, page.tsx, AboutMe.tsx and Hero.tsx

--- components/Home/AboutMe.tsx
+++ components/Home/AboutMe.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 const AboutMe = () => {
   return (
     <>
-      <div className="flex py-10 flex-col">
+      <div className="flex py-10 flex-col p-4">
         <img
           src="/mainAboutMe.jpg"
           alt="a picture of a man standing, holding a camera in his hands, looking in the camera and smiling"
-          className="rounded-full border"
+          className="rounded-full outline outline-offset-[12px] outline-slate-500 outline-1 object-cover self-center max-w-sm"
         />
+        <h3 className="subheading pt-12">about me</h3>
+        <h2 className="heading">
+          We're the leading photo studio in the country
+        </h2>
+        <p className="paragraph">
+          It is a long established fact that a reader will be distr by the
+          readabll content of a page when looking at its layout. The point of
+          using Lorel Ipsum is that it has a more-or-less normal distribution of
+          letters oppo to using Content here, content here making it look like
+          readabl Englis Many desktop publishing packages and.
+        </p>
+        <button className="btn btn-fill">
+          Learn More
+          <BsArrowRight className="text-xl" />
+        </button>
       </div>
     </>
   );
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -6,23 +6,19 @@ const Hero = () => {
     <>
       <div className="bg-center bg-auto bg-[url('/hero.jpg')] p-4">
         <div className="flex flex-col py-12">
-          <h3 className="uppercase tracking-widest font-medium">
-            welcome to pixoto world
-          </h3>
-          <h1 className="font-medium text-3xl pt-3">
-            We are a digital photography house
-          </h1>
-          <p className="pt-6 pb-10">
+          <h3 className="subheading">welcome to pixoto world</h3>
+          <h1 className="heading">We are a digital photography house</h1>
+          <p className="paragraph">
             Discover the world of digital photography with Pixoto. We specialize
             in capturing moments that matter, turning them into lasting
             memories. Explore our gallery and let images speak.{" "}
           </p>
           <div className="flex flex-col gap-3 font-medium">
-            <button className="rounded-md bg-slate-50 text-slate-900 border-2 border-transparent px-6 py-3 w-max flex items-center flex-row gap-2">
+            <button className="btn btn-fill">
               Get in touch
               <BsArrowRight className="text-xl" />
             </button>
-            <button className="rounded-md bg-transparent border-2 border-slate-50 text-slate-50 px-6 py-3 w-max flex items-center flex-row gap-2">
+            <button className="btn btn-outline">
               Read More
               <BsArrowRight className="text-xl" />
             </button>
