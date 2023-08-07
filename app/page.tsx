edb1878cb6dedc07ea18fdd6357eb51d9e1e209f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-07 13:08:42 +0000

    🌿: Monday, August 7, 2023 at 4:08:42 PM Eastern European Summer Time

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import AboutMe from "@/components/Home/AboutMe";
 import Hero from "@/components/Home/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-slate-900 text-slate-50 flex flex-col">
+      <div className="min-h-screen bg-slate-800 text-slate-50 flex flex-col">
         <Navbar />
         <Hero />
+        <AboutMe />
       </div>
     </>
   );
---
+++ components/Home/AboutMe.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <>
+      <div className="flex py-10 flex-col">
+        <img
+          src="/mainAboutMe.jpg"
+          alt="a picture of a man standing, holding a camera in his hands, looking in the camera and smiling"
+          className="rounded-full border"
+        />
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
