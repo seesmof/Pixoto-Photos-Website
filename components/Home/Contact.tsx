890057988c624211bb33a7e3fdc44d468440a5c2
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-10 10:56:29 +0300

    update Contact.tsx

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,32 +1,6 @@
 "use client";
 import React from "react";
-import { AiOutlineMail, AiOutlinePhone, AiOutlineHome } from "react-icons/ai";
-
-const ContactSubsection = ({ heading, icon, content }: any) => {
-  const IconComponent =
-    icon === "phone"
-      ? AiOutlinePhone
-      : icon === "mail"
-      ? AiOutlineMail
-      : AiOutlineHome;
-
-  return (
-    <>
-      <div className="flex flex-col gap-1">
-        <h3 className="font-medium">{heading}</h3>
-        <button
-          onClick={() => {
-            navigator?.clipboard?.writeText(content);
-          }}
-          className="flex flex-row items-center gap-2"
-        >
-          {IconComponent && <IconComponent className="text-xl stroke-[3]" />}
-          {content}
-        </button>
-      </div>
-    </>
-  );
-};
+import { ContactSubsection } from "./ContactSubsection";
 
 const Contact = () => {
   const ContactInfo = [
@@ -49,12 +23,13 @@ const Contact = () => {
 
   return (
     <>
-      <div className="flex z-10 relative py-10 flex-col p-4 bg-[url('/mainContact.png')] bg-auto bg-center">
-        <div className="absolute z-10 inset-0 bg-slate-900/30"></div>
-        <div className="z-20">
-          <h3 className="subheading">Simple and Convenient Steps</h3>
-          <h2 className="heading">Require Assistance from Our Photographer?</h2>
-          <div className="flex flex-col mt-6 bg-backgroundLight rounded-md p-4 gap-4">
+      <div className="py-10 bg-[url('/mainContact.png')] bg-auto bg-center">
+        <div className="flex flex-col md:flex-row md:gap-8 layout">
+          <div className="w-max">
+            <h3 className="subheading">Simple and Convenient Steps</h3>
+            <h2 className="heading">Need Assistance from Our Photographer?</h2>
+          </div>
+          <div className="flex w-full flex-col mt-6 bg-backgroundLight rounded-md p-4 gap-4">
             {ContactInfo.map((info) => (
               <ContactSubsection {...info} key={info.heading} />
             ))}
