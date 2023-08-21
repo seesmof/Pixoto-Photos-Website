ee19c57e5df88cb78c7b7c00610b3a1283c69503
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-21 16:51:56 +0300

    update 21 files and delete 1 file

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,21 @@
 "use client";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { ThemeToggle } from "@/components/theme-toggle";
+
+function TypographyBlockquote() {
+  return (
+    <blockquote className="mt-6 border-l-2 pl-6 italic">
+      "After all," he said, "everyone enjoys a good joke, so it's only fair that
+      they should pay for the privilege."
+    </blockquote>
+  );
+}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-4 gap-4">
-      <div className="flex justify-end w-full">
-        <ThemeToggle />
-      </div>
-
-      <Accordion type="single">
-        <AccordionItem value="first">
-          <AccordionTrigger>Why choose us?</AccordionTrigger>
-          <AccordionContent>
-            <p>Well, because we're the best.</p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="second">
-          <AccordionTrigger>How much do we charge?</AccordionTrigger>
-          <AccordionContent>
-            <p>We don't charge anything. Just kidding. We charge a lot.</p>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <ThemeToggle />
+      <TypographyBlockquote />
     </main>
   );
 }
