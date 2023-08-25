cd94523eeff014eaf83cd81a9106d3e805328e86
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-25 13:20:43 +0000

    🌿: Friday, August 25, 2023 at 4:20:43 PM Eastern European Summer Time

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,23 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import {
   NavigationMenu,
   NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
@@ -61,88 +73,100 @@ const MainPage = () => {
   return (
     <>
       <section className="grid bg-cover bg-no-repeat bg-[url('/assets/main-hero-background.jpg')] p-4 bg-center">
-        <nav className="flex justify-between items-center">
-          <h1 className="text-2xl font-bold">
-            <Link href="/">Pixoto</Link>
-          </h1>
-          <div className="flex flex-row items-center gap-1">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant={"ghost"} size={"icon"}>
-                  <ShoppingCart size={22} />
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left">
-                <SheetHeader>
-                  <SheetTitle>Your cart</SheetTitle>
-                  <SheetDescription>
-                    Add items to your cart and they will appear here
-                  </SheetDescription>
-                </SheetHeader>
-                <Separator className="my-2" />
-              </SheetContent>
-            </Sheet>
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant={"ghost"} size={"icon"}>
-                  <Menu size={22} />
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left">
-                <div className="grid gap-4">
-                  <Link href="/">Home</Link>
-                  <Link href="/">About</Link>
-                  <Link href="/">Shop</Link>
-                  <Collapsible>
-                    <CollapsibleTrigger
-                      className="flex flex-row items-center justify-between w-full pr-6"
-                      onClick={() => setIsPagesCollapsed(!isPagesCollapsed)}
-                    >
-                      Pages
-                      {isPagesCollapsed ? (
-                        <ChevronDown size={22} />
-                      ) : (
-                        <ChevronUp size={22} />
-                      )}
-                    </CollapsibleTrigger>
-                    <CollapsibleContent className="grid gap-4 ml-6">
-                      <Link href="/" className="pt-4">
-                        Portfolio
-                      </Link>
-                      <Link href="/">Services</Link>
-                      <Link href="/">Styles Guide</Link>
-                    </CollapsibleContent>
-                  </Collapsible>
-                  <Link href="/">Blog</Link>
-                  <Link href="/">Contact</Link>
-                </div>
-              </SheetContent>
-            </Sheet>
-          </div>
-        </nav>
+        <div className="max-w-6xl mx-auto">
+          <nav className="flex justify-between items-center lg:pt-6">
+            <h1 className="text-2xl lg:text-4xl font-bold">
+              <Link href="/">Pixoto</Link>
+            </h1>
+            <Menubar>
+              <MenubarMenu>
+                <MenubarTrigger>Trigger Button</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>Title</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
+            <div className="flex flex-row items-center gap-1">
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant={"ghost"} size={"icon"}>
+                    <ShoppingCart size={22} />
+                  </Button>
+                </SheetTrigger>
+                <SheetContent side="left">
+                  <SheetHeader>
+                    <SheetTitle>Your cart</SheetTitle>
+                    <SheetDescription>
+                      Add items to your cart and they will appear here
+                    </SheetDescription>
+                  </SheetHeader>
+                  <Separator className="my-2" />
+                </SheetContent>
+              </Sheet>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant={"ghost"} size={"icon"}>
+                    <Menu size={22} />
+                  </Button>
+                </SheetTrigger>
+                <SheetContent side="left">
+                  <div className="grid gap-4">
+                    <Link href="/">Home</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Shop</Link>
+                    <Collapsible>
+                      <CollapsibleTrigger
+                        className="flex flex-row items-center justify-between w-full pr-6"
+                        onClick={() => setIsPagesCollapsed(!isPagesCollapsed)}
+                      >
+                        Pages
+                        {isPagesCollapsed ? (
+                          <ChevronDown size={22} />
+                        ) : (
+                          <ChevronUp size={22} />
+                        )}
+                      </CollapsibleTrigger>
+                      <CollapsibleContent className="grid gap-4 ml-6">
+                        <Link href="/" className="pt-4">
+                          Portfolio
+                        </Link>
+                        <Link href="/">Services</Link>
+                        <Link href="/">Styles Guide</Link>
+                      </CollapsibleContent>
+                    </Collapsible>
+                    <Link href="/">Blog</Link>
+                    <Link href="/">Contact</Link>
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
+          </nav>
 
-        <div className="grid py-24">
-          <h2 className="uppercase font-medium">welcome to pixoto world</h2>
-          <h1 className="text-3xl font-bold mt-2">
-            We are a digital photography powerhouse
-          </h1>
-          <p className="mt-6">
-            Opposite plain whistle taken better missing ordinary egg fairly
-            palace tank indicate date bottom cave experience word service barn
-            orbit it news location compass
-          </p>
-          <div className="grid mt-8 gap-3">
-            <Button className="flex flex-row items-center gap-2 w-max">
-              Get in touch
-              <ArrowRight size={18} strokeWidth={1.4} />
-            </Button>
-            <Button
-              variant="outline"
-              className="flex flex-row items-center gap-2 w-max bg-transparent border border-slate-200"
-            >
-              Read more
-              <ArrowRight size={18} strokeWidth={1.4} />
-            </Button>
+          <div className="grid py-24 lg:py-48">
+            <h2 className="uppercase font-medium lg:text-lg">
+              welcome to pixoto world
+            </h2>
+            <h1 className="text-3xl lg:text-5xl font-bold lg:font-semibold lg:max-w-xl lg:leading-tight mt-2">
+              We are a digital photography powerhouse
+            </h1>
+            <p className="mt-6">
+              Opposite plain whistle taken better missing ordinary egg fairly
+              palace tank indicate date bottom cave experience word service barn
+              orbit it news location compass
+            </p>
+            <div className="grid mt-8 gap-3">
+              <Button className="flex flex-row items-center gap-2 w-max">
+                Get in touch
+                <ArrowRight size={18} strokeWidth={1.4} />
+              </Button>
+              <Button
+                variant="outline"
+                className="flex flex-row items-center gap-2 w-max bg-transparent border border-slate-200"
+              >
+                Read more
+                <ArrowRight size={18} strokeWidth={1.4} />
+              </Button>
+            </div>
           </div>
         </div>
       </section>
